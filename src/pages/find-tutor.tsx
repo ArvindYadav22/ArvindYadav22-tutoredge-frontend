c38aa327ace04159b4158d00416eeda1a037da57
ArvindYadav22 <155368175+ArvindYadav22@users.noreply.github.com>
2025-10-15 22:17:38 +0530

    commit

---
+++ src/pages/find-tutor.tsx
@@ -0,0 +1,11 @@
+import dynamic from 'next/dynamic';
+
+const TutorSearchPage = dynamic(() => import('@/components/find-tutor/TutorSearchPage'), { ssr: false });
+
+export default function FindTutorPage() {
+  return <TutorSearchPage />;
+}
+
+
+
+
